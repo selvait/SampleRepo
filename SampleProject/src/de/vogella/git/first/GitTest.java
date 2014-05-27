b93b1eb86d3e9e7d333880061f08e7811757e331
Selva <selvait@gmail.com>
2014-05-27 20:36:30 +0530

    Initial Commit

---
+++ SampleProject/src/de/vogella/git/first/GitTest.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package de.vogella.git.first;
+
+/**
+ * @author Selva
+ *
+ */
+public class GitTest {
+
+	public static void main(String arg[]){
+		System.out.println("Git is fun");
+	}
+}
