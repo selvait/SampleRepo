3c95a8e90ebe858bbd2c8085d8488e013077fcfb
Selva <selvait@gmail.com>
2014-05-27 23:00:48 +0530

    Added new Class

---
+++ SampleProject/src/de/vogella/git/first/Account.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package de.vogella.git.first;
+
+/**
+ * @author Selva
+ *
+ */
+public class Account {
+	private int i=10;
+
+	public int getI() {
+		return i;
+	}
+
+	public void setI(int i) {
+		this.i = i;
+	}
+}
